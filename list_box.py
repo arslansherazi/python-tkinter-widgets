913796e144f89cee7b12ac2a4b9eb3c1748a4b5c
Arslan <arslanhaider.dev@hotmail.com>
2023-04-02 03:14:17 +0500

    rename programming languages list variable

--- list_box.py
+++ list_box.py
@@ -10,38 +10,38 @@ win.resizable(0, 0)
 scrollbar = Scrollbar(win)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-List = Listbox(win, bg="blue", bd=10, highlightcolor="orange", fg="pink", selectbackground="Chartreuse", cursor="heart",
-               width="100", yscrollcommand=scrollbar.set)  # bd=border size
+programming_languages = Listbox(win, bg="blue", bd=10, highlightcolor="orange", fg="pink", selectbackground="Chartreuse",
+                                cursor="heart", width="100", yscrollcommand=scrollbar.set)  # bd=border size
 # selectbackground property sets the color when we select an element from the list.
 # cursor property sets the cursor when we move our mouse to List box or selects any element from ListBox.
 
 
-List.insert(1, "C")
+programming_languages.insert(1, "C")
 
-List.insert(2, "C++")
+programming_languages.insert(2, "C++")
 
-List.insert(3, "Java")
+programming_languages.insert(3, "Java")
 
-List.insert(4, "Cobol")
+programming_languages.insert(4, "Cobol")
 
-List.insert(5, "C")
+programming_languages.insert(5, "C")
 
-List.insert(6, "C++")
+programming_languages.insert(6, "C++")
 
-List.insert(7, "Java")
+programming_languages.insert(7, "Java")
 
-List.insert(8, "Cobol")
+programming_languages.insert(8, "Cobol")
 
-List.insert(9, "C")
+programming_languages.insert(9, "C")
 
-List.insert(10, "C++")
+programming_languages.insert(10, "C++")
 
-List.insert(11, "Java")
+programming_languages.insert(11, "Java")
 
-List.insert(12, "Cobol")
+programming_languages.insert(12, "Cobol")
 
-List.pack()  # List should be packed or placed or grid after done inserting elements in it.
+programming_languages.pack()  # List should be packed or placed or grid after done inserting elements in it.
 
-scrollbar.config(command=List.yview)
+scrollbar.config(command=programming_languages.yview)
 
 win.mainloop()
